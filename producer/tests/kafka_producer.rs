d4afb83bf81a540331a66aa0f1d1c6ffb188c9e9
Umran Hussain <umran.hussein@gmail.com>
2024-06-27 13:38:39 -0700

    created an integration test for KafkaProducer

---
+++ producer/tests/kafka_producer.rs
@@ -0,0 +1,43 @@
+use producer::{KafkaProducer, KafkaProducerMessage};
+
+const BROKERS: &'static str = "localhost:9092";
+
+#[tokio::test]
+async fn it_produces_messages() {
+    let n: u64 = 1000;
+
+    let producer = KafkaProducer::new(BROKERS.into());
+    let (tx, mut rx) = producer.produce().unwrap();
+
+    let payload: String = std::iter::repeat('c').take(144).collect();
+
+    // clone tx to ensure the sender is not dropped when the
+    // messages are all buffered and the task exits.
+    let tx_clone = tx.clone();
+    tokio::spawn(async move {
+        tracing::info!("queueing {} 144 byte payloads", n);
+
+        for prev_lsn in 1..=n {
+            tx_clone
+                .send(KafkaProducerMessage {
+                    topic: "test".into(),
+                    partition_key: "some_key".into(),
+                    prev_lsn,
+                    payload: payload.clone(),
+                })
+                .await
+                .unwrap();
+        }
+    });
+
+    let mut delivered = 0;
+
+    while let Some(_) = rx.recv().await {
+        delivered += 1;
+        if delivered == n {
+            break;
+        }
+    }
+
+    assert_eq!(delivered, n)
+}
